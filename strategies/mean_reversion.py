e47c2caa6d24b406ac2513b16498772c4d13b04d
Ish2K <ishaan.gupta04@hotmail.com>
2022-09-06 00:19:13 +0530

    events added

---
+++ strategies/mean_reversion.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from dataclasses import dataclass,asdict,astuple,field
+
+@dataclass
+class MeanReversion():
+    '''
+    Add Mean Reversion Strategy Description Here
+    '''
+    pass
